d92c7ec8db4658fbbfa0269972f132c054b131d3
pravin <rpraveenkum@gmail.com>
2025-12-27 14:13:41 -0600

    Configure aesd-assignments package with new source hash and project_base name

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,28 +1,30 @@
-
-##############################################################
-#
-# AESD-ASSIGNMENTS
-#
-##############################################################
-
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
-# Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+# TODO: Fill up the contents below in order to reference your assignment 3 git contents
+# Use the full 40-character git commit hash here
+AESD_ASSIGNMENTS_VERSION = b6e38b3256d30162aa4a647082289c99af22694e
+# Example: git@github.com:cu-ecen-aeld/assignments-3-and-later-YOURUSERNAME.git
+AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-pkrkrish.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+        # 1. Install executables and scripts to /usr/bin
+        $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+        $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+        $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+        $(INSTALL) -m 0755 $(@D)/assignment-autotest/tester.sh $(TARGET_DIR)/usr/bin/
+
+        # 2. Install configuration files to /etc/finder-app/conf/
+        $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf/
+        $(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+
+        # 3. Also include assignment-autotest/test/assignment4 content if needed by your scripts
+        # Note: Instructions usually imply these go into /usr/bin for global PATH access
+        $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
