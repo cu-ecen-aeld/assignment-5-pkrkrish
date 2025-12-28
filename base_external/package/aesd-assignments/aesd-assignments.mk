fb723c2982f6048b6c28d8d7026ad948eff40431
pravin <rpraveenkum@gmail.com>
2025-12-28 11:17:27 -0600

    assignment-4-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -12,20 +12,27 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	# Create necessary directories
+	# 1. Create necessary directories
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/bin
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf
+	# Added: Strict permissions for SSH and Dropbear
+	$(INSTALL) -d -m 0700 $(TARGET_DIR)/root/.ssh
+	$(INSTALL) -d -m 0700 $(TARGET_DIR)/etc/dropbear
 
-	# Install binaries and scripts
+	# 2. Install binaries and scripts
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/full-test.sh $(TARGET_DIR)/usr/bin/
 
-	# Install configuration files - match what finder-test.sh expects
+	# 3. Install configuration files
 	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	
-	# Install assignment-specific test scripts for the autograder
+
+	# 4. Install autograder test scripts
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
+
+	# 5. INSTALL SSH KEY (Requirement for automated login)
+	# We use PKGDIR because authorized_keys is in your Assignment 4 repository folder
+	$(INSTALL) -m 0600 $(AESD_ASSIGNMENTS_PKGDIR)/authorized_keys $(TARGET_DIR)/root/.ssh/authorized_keys
 endef
 $(eval $(generic-package))
