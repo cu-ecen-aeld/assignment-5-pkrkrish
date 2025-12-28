20692a2008d202ac88cf6699a26b57bed7b4adc4
pravin <rpraveenkum@gmail.com>
2025-12-27 18:46:18 -0600

    assignment-4-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,30 +1,41 @@
 # TODO: Fill up the contents below in order to reference your assignment 3 git contents
 # Use the full 40-character git commit hash here
-AESD_ASSIGNMENTS_VERSION = b6e38b3256d30162aa4a647082289c99af22694e
+AESD_ASSIGNMENTS_VERSION = ca49cc4a107a0fbaaf31c772640a7f57cbf55f79
 # Example: git@github.com:cu-ecen-aeld/assignments-3-and-later-YOURUSERNAME.git
 AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-pkrkrish.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-        # 1. Install executables and scripts to /usr/bin
-        $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
-        $(INSTALL) -m 0755 $(@D)/assignment-autotest/tester.sh $(TARGET_DIR)/usr/bin/
+	# Remove any existing file/link named dropbear to prevent the 'File exists' error
+	rm -rf $(TARGET_DIR)/etc/dropbear
+	
+	# Create directories (mkdir -p won't fail if the directory exists, 
+	# but it fails if a FILE with that name exists, which is why we rm -rf above)
+	mkdir -p $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/usr/conf
+	mkdir -p $(TARGET_DIR)/etc/finder-app/conf
+	mkdir -p $(TARGET_DIR)/etc/dropbear
 
-        # 2. Install configuration files to /etc/finder-app/conf/
-        $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/finder-app/conf/
-        $(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	# Install binaries/scripts
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/full-test.sh $(TARGET_DIR)/usr/bin/
 
-        # 3. Also include assignment-autotest/test/assignment4 content if needed by your scripts
-        # Note: Instructions usually imply these go into /usr/bin for global PATH access
-        $(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
+	# Install configuration files
+	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/usr/conf/
+	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+
+	# Install assignment-specific test scripts
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
+	# 1. Clean and Create the directory with strict 700 permissions
+	rm -rf $(TARGET_DIR)/etc/dropbear
+	$(INSTALL) -d -m 0700 $(TARGET_DIR)/etc/dropbear
 endef
-
 $(eval $(generic-package))
