e699a611a9ec1d47ce696f0c6775724103550e4a
pravin <rpraveenkum@gmail.com>
2025-12-29 19:23:11 -0600

    assignment-5 part2-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,39 +1,45 @@
 # TODO: Fill up the contents below in order to reference your assignment 3 git contents
 # Use the full 40-character git commit hash here
-AESD_ASSIGNMENTS_VERSION = ca49cc4a107a0fbaaf31c772640a7f57cbf55f79
+AESD_ASSIGNMENTS_VERSION = 307741399b9b9a8f0ab8273b70cbc9de4396a3ca
 # Example: git@github.com:cu-ecen-aeld/assignments-3-and-later-YOURUSERNAME.git
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-pkrkrish.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	# Build finder-app from previous assignments
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	# Build the new aesdsocket application from the server directory
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	# 1. Create directories
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/init.d
 	mkdir -p $(TARGET_DIR)/etc/finder-app/conf
 	mkdir -p $(TARGET_DIR)/root/.ssh
 
-	# 2. Set strict permissions (The critical fix)
+	# 2. Set strict permissions
 	chmod 0700 $(TARGET_DIR)/root
 	chmod 0700 $(TARGET_DIR)/root/.ssh
 
-	# 3. Install binaries and scripts
+	# 3. Install binaries and scripts (Updated for Assignment 5)
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/full-test.sh $(TARGET_DIR)/usr/bin/
+	# Install the aesdsocket binary
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
 
 	# 4. Install configuration files
 	$(INSTALL) -m 0644 $(@D)/finder-app/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 
-	# 5. Install autograder test scripts
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin/
+	# 5. Install init scripts (Required for Assignment 5)
+	# This installs your start-stop script as an auto-starting daemon
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
-	# 6. Install SSH KEY (Verify this filename matches exactly in your repo)
+	# 6. Install SSH KEY
 	$(INSTALL) -m 0600 $(AESD_ASSIGNMENTS_PKGDIR)/authorized_keys $(TARGET_DIR)/root/.ssh/authorized_keys
 endef
 $(eval $(generic-package))
